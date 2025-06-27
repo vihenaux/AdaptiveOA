6805a15423ac189b3e58e5726b6c9337b8b36513
vihenaux <henauxvincent@gmail.com>
2025-06-27 23:04:43 +0200

    Add pivot rule concept

---
+++ src/algorithms/local_search_specifics/PivotRuleBase.hpp
@@ -0,0 +1,25 @@
+#include <concepts>
+#include <optional>
+
+
+namespace AdaptiveOA {
+
+    template<
+            typename Rule,
+            typename Neighborhood,
+            typename Solution,
+            typename Function
+    >
+    concept PivotRuleLike =
+            NeighborhoodLike<Neighborhood> &&
+            SolutionLike<Solution> &&
+            FunctionLike<Function> &&
+            requires(Rule rule, Neighborhood& nh, Solution& sol, const Function& f)
+    {
+        typename Neighborhood::Mutation;
+        requires MutationLike<Solution, typename Neighborhood::Mutation>;
+
+        { rule.choose(nh, sol, f) } -> std::same_as<std::optional<typename Neighborhood::Mutation>>;
+    };
+
+} // namespace AdaptiveOA
