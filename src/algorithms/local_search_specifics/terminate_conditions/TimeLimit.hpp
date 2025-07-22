85751b0956383331016498fa450d9b8b4aefe708
Vincent Henaux <henauxvincent@gmail.com>
2025-07-22 19:26:07 +0200

    Reduce now calls to reduce sys time

--- src/algorithms/local_search_specifics/terminate_conditions/TimeLimit.hpp
+++ src/algorithms/local_search_specifics/terminate_conditions/TimeLimit.hpp
@@ -9,19 +9,26 @@ namespace AdaptiveOA
         public:
 
         TimeLimit() = delete;
-        TimeLimit(std::size_t micro_second_limit) : m_max_duration(std::chrono::microseconds(micro_second_limit)), m_start(std::chrono::high_resolution_clock::now()) {}
+        TimeLimit(std::size_t micro_second_limit) : m_max_duration(std::chrono::microseconds(micro_second_limit)), m_start(std::chrono::steady_clock::now()) {}
 
         template<FunctionLike Function>
         void update(const Function&) {}
 
-        bool should_terminate() const
+        bool should_terminate()
         {
-            return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_start) >= m_max_duration;
+            ++m_count;
+            if(m_count >= 1000)
+            {
+                m_count = 0;
+                return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - m_start) >= m_max_duration;
+            }
+            return false;
         }
 
         private:
 
         std::chrono::microseconds m_max_duration;
-        std::chrono::high_resolution_clock::time_point m_start;
+        std::chrono::steady_clock::time_point m_start;
+        std::size_t m_count{0};
     };
 }
