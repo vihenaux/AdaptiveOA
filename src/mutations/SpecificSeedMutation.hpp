24464d29a91fcead204bcb5bea908b1acf5eb5c8
Vincent Henaux <henauxvincent@gmail.com>
2025-08-26 23:29:31 +0200

    Add specialization of SpecificSeed[Neighborhood|Mutation] for the NKModel

--- src/mutations/SpecificSeedMutation.hpp
+++ src/mutations/SpecificSeedMutation.hpp
@@ -5,6 +5,8 @@
 namespace AdaptiveOA
 {
 
+    class NKModel;
+
     template<typename T>
     class SpecificSeedMutation : public SeedMutation
     {
@@ -17,6 +19,35 @@ namespace AdaptiveOA
         {
             return SeedMutation::operator==(mutation);
         }
+    };
+
+    template<>
+    class SpecificSeedMutation<NKModel> : public SeedMutation
+    {
+        public:
+
+        SpecificSeedMutation() = delete;
+        SpecificSeedMutation(unsigned int seed, unsigned int nb_matrix_mutated, unsigned int nb_links_mutated) : SeedMutation(seed), m_nb_matrix_mutated(nb_matrix_mutated), m_nb_links_mutated(nb_links_mutated) {}
+
+        unsigned int get_nb_matrix_mutated() const
+        {
+            return m_nb_matrix_mutated;
+        }
+
+        unsigned int get_nb_links_mutated() const
+        {
+            return m_nb_links_mutated;
+        }
+
+        bool operator==(const SpecificSeedMutation<NKModel>& mutation) const
+        {
+            return SeedMutation::operator==(mutation);
+        }
+
+        private:
+
+        unsigned int m_nb_matrix_mutated;
+        unsigned int m_nb_links_mutated;
     };
 
 }
--- src/neighborhoods/SpecificSeedNeighborhood.hpp
+++ src/neighborhoods/SpecificSeedNeighborhood.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "../core/NeighborhoodBase.hpp"
-#include "../mutations/SeedMutation.hpp"
+#include "../mutations/SpecificSeedMutation.hpp"
 #include "../utils/Random.hpp"
 
 namespace AdaptiveOA
 {
+
+    class NKModel;
 
     template<typename T>
     class SpecificSeedNeighborhood : public NeighborhoodBase<SpecificSeedNeighborhood<T>, SpecificSeedMutation<T>>
@@ -31,5 +33,39 @@ namespace AdaptiveOA
         void do_reset() const {}
     };
 
+    template<>
+    class SpecificSeedNeighborhood<NKModel> : public NeighborhoodBase<SpecificSeedNeighborhood<NKModel>, SpecificSeedMutation<NKModel>>
+    {
+        public:
+
+        friend NeighborhoodBase<SpecificSeedNeighborhood<NKModel>, SpecificSeedMutation<NKModel>>;
+
+        private:
+
+        bool do_mutation_available() const
+        {
+            return true;
+        }
+
+        SpecificSeedMutation<NKModel> do_next_mutation()
+        {
+            return SpecificSeedMutation<NKModel>{ Random::get_uint() , Random::get_fast_binomial_distribution(m_last_matrix_mutation) , Random::get_fast_binomial_distribution(m_last_links_mutation) };
+        }
+
+        void do_accept_mutation(const SpecificSeedMutation<NKModel>& m)
+        {
+            m_last_matrix_mutation = (m.get_nb_matrix_mutated() == 0) ? 1 : m.get_nb_matrix_mutated();
+            m_last_links_mutation = (m.get_nb_links_mutated() == 0) ? 1 : m.get_nb_links_mutated();
+        }
+
+        void do_reset()
+        {
+            m_last_matrix_mutation = 1;
+            m_last_links_mutation = 1;
+        }
+
+        unsigned int m_last_matrix_mutation{1};
+        unsigned int m_last_links_mutation{1};
+    };
 
 } // namespace AdaptiveOA
